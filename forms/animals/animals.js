140617335d7a57b9abdc229361d71d1c9c914d2b
Jacob Taxdahl <jacobtaxdahl9@gmail.com>
2020-10-05 16:10:22 -0500

    midterm1

---
+++ forms/animals/animals.js
@@ -0,0 +1,9 @@
+/*
+let animals = ["dog","cat","horse","meercat"]
+
+let newAnimal = prompt("Enter an animal to add to an array")
+let lastAnimal = newAnimal.toLowerCase()
+
+animals.push(lastAnimal)
+alert(`The last animal is ${animals[animals.length - 1]}`)
+*/
---
+++ forms/incomeBracket/incomeBracket.js
@@ -0,0 +1,15 @@
+btSubmit.onclick=function(){
+  let income = intIncome.value
+  let bracket = `With your income of $${income}, you are in a tax bracket of `
+if (income < 30000) {
+   lblBracket.value = bracket + '8%'
+    } else if (income < 99999 && income >= 30000) {
+   lblBracket.value = bracket + '15%'
+    } else if (income >= 99999) {
+   lblBracket.value = bracket + '25%'
+  }
+}
+
+btGoAgain.onclick=function(){
+  lblBracket.value = "Enter another income"
+}
